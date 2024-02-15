8b1cbdcad2e64d1b487b9182871ec309cdde967e
sureshreddyrayapati <resuresh0@gmail.com>
2024-02-15 15:15:22 +0530

    FirstCommit

---
+++ CheckPalindrome.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolindromNumber
+{
+    public class CheckPalindrome
+    {
+        public string Paly(int n)
+        {
+            string ms = "";
+            if (n < 0)
+            {
+                n = -n;
+            }
+            if (n == 0)
+            {
+                ms = "Inavlid Number";
+            }
+            else
+            {
+                int temp = n, rev = 0;
+                while (n > 0)
+                {
+                    int r = n % 10;
+                    rev = rev * 10 + r;
+                    n = n / 10;
+                }
+                if (rev == temp)
+                {
+                    ms = "The Number is Palindrome";
+                }
+                else
+                {
+                    ms = "The Number is not a palindrome";
+                }
+            }
+            return ms;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using PolindromNumber;
+
+Console.WriteLine("Check a number is palindrome or not");
+CheckPalindrome obj = new CheckPalindrome();
+Console.WriteLine(obj.Paly(125));
